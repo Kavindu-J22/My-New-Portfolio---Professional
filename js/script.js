d280c9b5d45ad1bc3bd4b43d0660a44580a7862a
Kavindu-J22 <it21032806@my.sliit.lk>
2023-03-10 04:35:28 +0530

    update

---
+++ js/script.js
@@ -0,0 +1,19 @@
+/* -- Scroll Section : Active Link --*/
+    let sections = document.querySelectorAll("section");
+    let navLinks = document.querySelectorAll("header nav a");
+
+    window.onscroll = () => {
+        sections.forEach(sec => {
+            let top = window.scrollY;
+            let offset = sec.offsetTop - 150;
+            let height = sec.offsetHight;
+            let id = sec.getAtribute('id');
+
+            if(top >= offset && top < offset + height) {
+                navLinks.forEach(links => {
+                    links.classList.remove("active");
+                    document.querySelector("header nav a[href*=" + id + "]").classList.add("active");
+                });
+            };
+        });
+    };
