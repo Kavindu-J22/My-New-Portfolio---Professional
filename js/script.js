ebdcf29dfffd0f31bd937d5d3f75045e095f6e20
Kavindu-J22 <it21032806@my.sliit.lk>
2023-03-12 08:44:12 +0530

    update

--- js/script.js
+++ js/script.js
@@ -52,7 +52,7 @@ menuIcon.onclick = () => {
     /* -- typed jsl --*/
 
     const typed = new Typed('.multiple-text',{
-        strings: ['Full-Stack Developer', 'UI/UX Designer', 'Grapic Designer', 'Document Creator', 'Software Engineere'],
+        strings: ['Full-Stack Developer', 'UI/UX Designer', 'Grapic Designer', 'Document Creator', 'Software Engineer'],
         typeSpeed: 100,
         backSpeed: 100,
         backDelay: 1000,
